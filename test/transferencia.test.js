33df6de5a293fe3554fb2b0fed68cec4ebd2b47b
Alana <alanareis10@gmail.com>
2025-09-16 23:31:19 -0300

    Adcionando a biblioteca .env para variaveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,9 +1,10 @@
-const request = require('supertest');
+const request = require('supertest');   
 const { expect } = require('chai')
+        require('dotenv').config()
 describe('Transferências', () =>{
     describe('POST /transferencias', () => {
         it('deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00',   async () => {
-           const respostaLogin = await request('http://localhost:3000')
+           const respostaLogin = await request(process.env.BASE_URL)
                            .post('/login')
                            .set('Content-Type', 'application/json')
                            .send({
@@ -13,7 +14,7 @@ describe('Transferências', () =>{
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json')
               .set('Authorization', `bearer ${token}`)
@@ -39,7 +40,7 @@ describe('Transferências', () =>{
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json')
               .set('Authorization', `bearer ${token}`)
