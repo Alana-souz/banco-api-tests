3cba8c0b386f77f8adcfb715b676479c97c6a874
Alana <alanareis10@gmail.com>
2025-09-17 17:17:16 -0300

    Adcionei teste para Get transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -4,12 +4,14 @@ require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
 const postTransferencias = require('../fixtures/postTransferencias.json')   
 describe('Transferências', () =>{
-    describe('POST /transferencias', () => {
-        let token;
+    let token;
         
         beforeEach(async () => {
             token = await obterToken('julio.lima', '123456')
         })
+
+    describe('POST /transferencias', () => {
+        
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00',   async () => {
            const bodyTransferencias = { ...postTransferencias }
 
@@ -34,9 +36,39 @@ describe('Transferências', () =>{
               .set('Content-Type', 'application/json')
               .set('Authorization', `bearer ${token}`)
               .send(bodyTransferencias)
-              
+
               expect(resposta.status).to.equal(422);
 
+        })
     })
-})
+    describe('GET /transferencias/{id}', () => {
+        it('deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando ID for válido', async () => {
+
+        const resposta = await request(process.env.BASE_URL)
+              .get('/transferencias/1')
+              .set('Authorization', `bearer ${token}`)
+
+            
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(1)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2)
+            expect(resposta.body.valor).to.equal(20.25)
+        })
+
+    })
+    describe('GET /transferencias', () => {
+        it('deve retornar 10 elementos na paginacao quando informar limite de 10 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+              .get('/transferencias?page=1&limit=10')
+              .set('Authorization', `bearer ${token}`)
+                expect(resposta.status).to.equal(200)
+                expect(resposta.body.limit).to.equal(10)
+                expect(resposta.body.transferencias).to.have.lengthOf(10)
+
+              console.log(resposta.body)
+             
+        })
+    })
 })
